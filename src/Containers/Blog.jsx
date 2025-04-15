8f4055d103ab67b8e8642eeeae6ebaa9a3683720
reuben148 <reubenluka555@gmail.com>
2025-04-15 09:46:05 +0100

    last push

--- src/Containers/Blog.jsx
+++ src/Containers/Blog.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Blog() {
   return (
-    <div>Blog</div>
-  )
+    <div className="flex flex-col md:flex-row justify-between items-center px-8 py-8 md:my-16 my-10 mx-8 md:mx-16 rounded-xl bg-[#6D6D6D]">
+      <div className="text-[#0E0E0E]">
+        <p className="font-medium text-[12px] leading-[30px] font-sans">
+        Let’s try our service now!
+        </p>
+        <h3 className="font-extrabold text-[24px] leading-[45px] font-sans text-black md:text-[24px] md:leading-[45px] text-[18px] leading-[32px]">
+        Everything you need to accept card payments and grow your business anywhere on the planet.
+        </h3>
+      </div>
+
+      <div className="flex justify-center items-center mt-8 md:mt-0 md:ml-8">
+        <button className="bg-black text-white rounded-full font-bold text-[18px] leading-[30px] py-2 px-4 min-w-[150px] hover:opacity-90 transition duration-300 md:text-[18px] md:leading-[30px] text-[14px] leading-[28px]">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;
--- src/Containers/Footer.jsx
+++ src/Containers/Footer.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-[#0A1C2C] text-white py-16 px-8">
+      {/* Logo and Address */}
+      <div className="flex flex-col sm:flex-row justify-between items-start mb-8">
+        <div className="flex flex-col items-start">
+          <img src="header.jpg" alt="Logo" className="w-32 mb-4" />
+          <p className="text-sm text-[#A0A0A0]">
+            1234 Street Name, City, Country
+          </p>
+        </div>
+      </div>
 
-export default Footer
+      {/* Links Section */}
+      <div className="flex flex-col sm:flex-row justify-between items-start mb-8">
+        {/* Quick Links */}
+        <div className="flex flex-col mb-8 sm:mb-0">
+          <h4 className="text-lg font-semibold mb-4">Quick Links</h4>
+          <a href="#" className="text-sm text-[#A0A0A0] mb-2">
+            Home
+          </a>
+          <a href="#" className="text-sm text-[#A0A0A0] mb-2">
+            About
+          </a>
+          <a href="#" className="text-sm text-[#A0A0A0] mb-2">
+            Services
+          </a>
+          <a href="#" className="text-sm text-[#A0A0A0] mb-2">
+            Contact
+          </a>
+        </div>
+
+        {/* Company Info */}
+        <div className="flex flex-col mb-8 sm:mb-0">
+          <h4 className="text-lg font-semibold mb-4">Company</h4>
+          <a href="#" className="text-sm text-[#A0A0A0] mb-2">
+            Privacy Policy
+          </a>
+          <a href="#" className="text-sm text-[#A0A0A0] mb-2">
+            Terms of Service
+          </a>
+        </div>
+
+        {/* Contact Details */}
+        <div className="flex flex-col mb-8 sm:mb-0">
+          <h4 className="text-lg font-semibold mb-4">Contact</h4>
+          <p className="text-sm text-[#A0A0A0] mb-2">
+            Email: contact@company.com
+          </p>
+          <p className="text-sm text-[#A0A0A0] mb-2">Phone: (123) 456-7890</p>
+        </div>
+      </div>
+
+      {/* Social Media Icons */}
+      <div className="flex justify-center sm:justify-start mb-8">
+        <a href="#" className="text-[#A0A0A0] mx-4">
+          Facebook
+        </a>
+        <a href="#" className="text-[#A0A0A0] mx-4">
+          Twitter
+        </a>
+        <a href="#" className="text-[#A0A0A0] mx-4">
+          Instagram
+        </a>
+      </div>
+
+      {/* Copyright Notice */}
+      <div className="text-center text-sm text-[#A0A0A0]">
+        <p>© 2025 Company Name. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
